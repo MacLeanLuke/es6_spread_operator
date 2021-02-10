cbd826b80f5600d8767ffcb154a2fa6677608da1
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-02-09 21:35:50 -0600

    using es6 spread operator to consolidate else if statements to three lines of code.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const citrus = ["lime", "lemon", "orange"];
+const fruits = ["apple", "banana", "coconut", ...citrus];
+
+const fullName = {
+  fName: "Jamees",
+  lname: "Bond"
+};
+
+const user = {
+  ...fullName,
+  id: 7,
+  username: "jamesbond007"
+};
+
+console.log(user);
